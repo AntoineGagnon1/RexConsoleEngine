311112ff87823b5a5447bed7f559e3a5bc5e1827
Antoine Gagnon <antoinegagnon123@gmail.com>
2021-09-07 22:53:42 -0400

    Better example (less colors...)

--- src/Example.cpp
+++ src/Example.cpp
@@ -20,11 +20,11 @@ int main()
 		{
 			for (int y = 0; y < c->GetHeight(); y++)
 			{
-				c->Draw(x,y, Console::Pixel(Console::Color::White, Console::Color::Cyan, rand() % 26 + 'a'));
+				c->Draw(x,y, Console::Pixel(Console::Color::Dark_Grey, Console::Color::Black, rand() % 26 + 'a'));
 			}
 		}
 
-		c->DrawString(50, 10, Console::Color::White, Console::Color::Cyan, L"Hello world !\ntest !\n\n\nhi !");
+		c->DrawString(50, 10, Console::Color::Green, Console::Color::Black, L"Hello world !\ntest !\n\n\nhi !");
 
 		int centerX = c->GetWidth() / 2;
 		int centerY = c->GetHeight() / 2;
@@ -34,10 +34,7 @@ int main()
 			c->DrawLine(centerX, centerY, centerX + cos(a * (3.14f / 180.0f)) * 25, centerY + sin(a * (3.14f / 180.0f)) * 25, Console::Color::Dark_Green);
 		}
 
-		if (c->IsPressed(Console::Key::MouseForward))
-			c->Draw(5,5, Console::Pixel(Console::Color::Red));
-
-		c->Draw(c->MouseX(), c->MouseY(), Console::Pixel(Console::Color::Yellow));
+		c->Draw(c->MouseX(), c->MouseY(), Console::Pixel(c->IsPressed(Console::Key::MouseLeft) ? Console::Color::Red : Console::Color::Yellow));
 
 		c->DrawString(0, 0, Console::Color::White, Console::Color::Black, std::to_wstring(1.0f / c->DeltaTime()));
 
